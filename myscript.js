66ff3918b8d62283008b906089d95728d84d3b31
moomme <momme_omer@hotmail.com>
2019-07-10 15:41:26 +0200

    Some more responsiveness and added a footer

--- myscript.js
+++ myscript.js
@@ -21,7 +21,7 @@ function sayHi(){
 //   }
 // }
 
-if (document.documentElement.clientWidth > 800){
+if (document.documentElement.clientWidth > 980){
     $(document).ready(function() {
         $("#hidden-navbar").hide(); //hide your div initially
         var topOfOthDiv = $("#navbar").offset().top;
@@ -63,3 +63,25 @@ function showSlides(n) {
 }
 
 window.onload = showSlides(slideIndex);
+
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.navbar-burger')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
