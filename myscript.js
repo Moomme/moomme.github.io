235cff2e9db36d1ad948e99e4979db9bf9d13502
moomme <momme_omer@hotmail.com>
2019-07-15 14:50:04 +0200

    dropdown navbar improvements

--- myscript.js
+++ myscript.js
@@ -66,22 +66,30 @@ window.onload = showSlides(slideIndex);
 
 
 
-/* When the user clicks on the button, 
+  
+//   // Close the dropdown menu if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.navbar-burger')) {
+//         $('#myDropdown').hide(200);
+//     //   var dropdowns = document.getElementsByClassName("dropdown-content");
+//     //   var i;
+//     //   for (i = 0; i < dropdowns.length; i++) {
+//     //     var openDropdown = dropdowns[i];
+//     //     if (openDropdown.classList.contains('show')) {
+//     //       openDropdown.classList.remove('show');
+//     //     }
+//     //   }
+//     }
+//   }
+
+  /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function dropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.navbar-burger')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+    /*document.getElementById("myDropdown").classList.toggle("show");*/
+    elem = document.getElementById("myDropdown");
+    if (window.getComputedStyle(elem).display == 'none'){
+        $('#myDropdown').show(200);
+    } else{
+        $('#myDropdown').hide(200);
     }
   }
